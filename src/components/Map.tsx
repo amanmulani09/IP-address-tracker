5ce01da8aa7b3a87c0509f3794db37dfc6a84844
amanmulani09 <mulaniaman0504@gmail.com>
2023-08-08 00:59:12 +0530

    created map comoponent

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,12 +2,29 @@
 import { MapContainer,TileLayer,Marker,Popup } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
+import ChangeMapView from "./ChangeMapView"
 import locationIcon from '/public/location.svg'
-const Map = ()=>{
+const Map = ({coordinates}:any)=>{
 
     return(
-        <>
-        </>
+        <div className=" h-2/3 z-0 relative">
+            <MapContainer 
+            center={coordinates} zoom={13} 
+            style={{width:'100%', height:'80vh'}}
+            >
+
+        <TileLayer 
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+       <Marker 
+        position={coordinates} 
+        icon={new Icon({iconUrl:locationIcon})}>
+        <Popup >Pretty Popup</Popup>
+      </Marker>
+      <ChangeMapView coordinates={coordinates}/>
+      </MapContainer>
+        </div>
     )
 }
 export default Map
