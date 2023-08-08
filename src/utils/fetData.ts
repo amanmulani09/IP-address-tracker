10f3ccc6deee7748e2755966deab60352b3cab99
amanmulani09 <mulaniaman0504@gmail.com>
2023-08-09 01:04:04 +0530

    added custom fetch functions

--- src/utils/fetData.ts
+++ src/utils/fetData.ts
@@ -8,16 +8,38 @@ const options = {
 	}
 };
 
-export const FetchData = async(city:string)=>{
-// const url = ``;
-
+export const FetchGeoData = async(city:string)=>{
+console.log(city,'city')
 try{
     const response = await fetch(`${url}${city}`,options);
-    const data = await response.json();
-    return data;
-
+    return response;
 }catch(error){
     console.log(error)
 }
 
+}
+
+export const fetchSTDCode = async(cityName:string)=>{
+    const options:any = {
+        method: 'POST',
+        url: 'https://std.p.rapidapi.com/',
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': '9968a37f2cmshc78d2bddf6b1a13p135af1jsndb166f7734d3',
+          'X-RapidAPI-Host': 'std.p.rapidapi.com'
+        },
+        data: {search: cityName}
+      };
+      
+      try {
+        console.log(cityName)
+          const response = await fetch(options)
+          console.log(response,'city respoc')
+          return response;
+
+      } catch (error) {
+          console.error(error);
+      }
+
+      
 }
